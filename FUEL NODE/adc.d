36ba0edcf222b71d5547ba2c055c92ce0e67146f
YamaPrashanth <yamaprashanth15@gmail.com>
2026-05-20 17:44:16 +0530

    Delete FUEL NODE directory

--- FUEL NODE/adc.d
+++
@@ -1,8 +0,0 @@
-.\adc.o: adc.c
-.\adc.o: headers.h
-.\adc.o: E:\KEIL\ARM\Inc\Philips\LPC21xx.h
-.\adc.o: types.h
-.\adc.o: defines.h
-.\adc.o: delay.h
-.\adc.o: pin_function_defines.h
-.\adc.o: adc_defines.h
