91182a6b953d0701d85d36bdf8a5d20e3d217e35
YamaPrashanth <yamaprashanth15@gmail.com>
2026-05-20 17:44:41 +0530

    Delete MAIN NODE directory

--- MAIN NODE/ds18b20.d
+++
@@ -1,8 +0,0 @@
-.\ds18b20.o: ds18b20.c
-.\ds18b20.o: headers.h
-.\ds18b20.o: E:\KEIL\ARM\Inc\Philips\LPC21xx.h
-.\ds18b20.o: types.h
-.\ds18b20.o: defines.h
-.\ds18b20.o: delay.h
-.\ds18b20.o: pin_function_defines.h
-.\ds18b20.o: adc_defines.h
